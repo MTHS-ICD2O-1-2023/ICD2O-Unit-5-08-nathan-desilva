e04aaa468f8d68bd543fb8655eb0accc1530bf54
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-10 18:06:35 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function myButtonClicked() {
+
+  // input
+  const numberA = parseInt(document.getElementById('number-a').value)
+  const numberB = parseInt(document.getElementById('number-b').value)
+  let remainder = numberA
+  let output = 0
+
+  // process
+
+  while (remainder >= numberB) {
+    remainder -= numberB
+    output++
+  }
+  if (remainder > 0) {
+    document.getElementById('answer').innerHTML = numberA + " ÷ " + numberB + " = " + output + " with a remainder of " + remainder + "."
+  } else {
+    document.getElementById('answer').innerHTML = numberA + " ÷ " + numberB + " = " + output + "."
+  }
+}
